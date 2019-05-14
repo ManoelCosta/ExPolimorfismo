9af069769213144e3652942cfa90e28dc4363b80
Manoel Costa <ManoelCosta>
2019-05-13 21:02:08 -0300

    Initial commit

---
+++ src/Compra.java
@@ -0,0 +1,11 @@
+public class Compra {
+    private Produto[] produtos = new Produto[10];
+
+    public void realizarCompra(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Mouse.java
@@ -0,0 +1,19 @@
+public class Mouse extends Produto{
+    private String tipo;
+
+    public Mouse(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+}
